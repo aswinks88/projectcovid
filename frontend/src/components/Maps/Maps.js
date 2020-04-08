a101365f7ca1baf50bbd66b0f1e7b4f5d6fd07a0
aswinks88 <aswinks88@gmail.com>
2020-04-08 16:46:25 +1200

    added colors to the region

--- frontend/src/components/Maps/Maps.js
+++ frontend/src/components/Maps/Maps.js
@@ -13,6 +13,30 @@ import zlib from 'zlib'
 import statesData from './nz-district-data.json'
 
 export default class Leaflet extends Component {
+    constructor(props){
+        super(props)
+        this.getColor = this.getColor.bind(this)
+        this.mapStyle = this.mapStyle.bind(this)
+    }
+    getColor(d){
+        return d > 200  ? '#E31A1C' :
+        d > 100  ? '#FC4E2A' :
+        d > 50   ? '#FD8D3C' :
+        d > 20   ? '#FEB24C' :
+        d > 10   ? '#FED976' :
+                   '#FFEDA0'
+    }
+    mapStyle(){
+        return {
+            fillColor: this.getColor(100),
+            weight: 2,
+            opacity: 1,
+            color: 'white',
+            dashArray: '3',
+            fillOpacity: 0.7
+
+        }
+    }
     componentDidMount(){
         this.map = L.map('map', {
             center: [-40.9006,174.886],
@@ -26,8 +50,9 @@ export default class Leaflet extends Component {
             maxZoom: 20,
             maxNativeZoom: 17
         }).addTo(this.map)
-        L.geoJSON(statesData).addTo(this.map)
+        L.geoJSON(statesData, {style: this.mapStyle}).addTo(this.map)
     }
+   
     render(){
         return (
            
